015011c3b34f8d566cb564c5e975dbc56a634c1d
Kristian Lyngstol <kly@kly.no>
2019-05-01 00:23:33 +0200

    Hooray! A test-case! More to come...

---
+++ pkg/parsers/json_test.go
@@ -0,0 +1,37 @@
+/*
+ * skogul, test json parser
+ *
+ * Copyright (c) 2019 Telenor Norge AS
+ * Author(s):
+ *  - Kristian Lyngstøl <kly@kly.no>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301  USA
+ */
+
+package parsers
+
+import "testing"
+
+// TestJSONParse tests parsing of a simple JSON document to skogul
+// container
+func TestJSONParse(t *testing.T) {
+	var b []byte = []byte("{\"metrics\":[{\"timestamp\":\"2019-03-15T11:08:02+01:00\",\"metadata\":{\"key\":\"value\"},\"data\":{\"string\":\"text\",\"float\":1.11,\"integer\":5}}]}")
+	x := JSON{}
+	_, err := x.Parse(b)
+	if err != nil {
+		t.Errorf("JSON.Parse(b) failed: %s", err)
+	}
+}
