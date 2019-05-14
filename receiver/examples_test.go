30e9f0e00eaf66274f4d00e3dbb06c5e44f244ab
Kristian Lyngstol <Kristian.Lyngstol@telenor.com>
2019-05-14 10:28:13 +0200

    Yet an other HTTP example

--- receiver/examples_test.go
+++ receiver/examples_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 /*
-HTTP can have different skogul.Handler's for different paths
+HTTP can have different skogul.Handler's for different paths, with potentially different behaviors.
 */
 func ExampleHTTP() {
 	h := receiver.HTTP{Address: "localhost:8080"}
@@ -17,4 +17,17 @@ func ExampleHTTP() {
 	noTemplate := skogul.Handler{Parser: parser.JSON{}, Sender: sender.Debug{}}
 	h.Handle("/template", &template)
 	h.Handle("/notemplate", &noTemplate)
+	h.Start()
+}
+
+/*
+Using New() sets up a single handler on the specified path. This is the same as
+*/
+func ExampleHTTP_new() {
+	handler := skogul.Handler{Parser: parser.JSON{}, Transformers: []skogul.Transformer{transformer.Templater{}}, Sender: sender.Debug{}}
+	h, err := receiver.New("http://localhost:8080/foobar", handler)
+	if err != nil {
+		panic(err)
+	}
+	h.Start()
 }
