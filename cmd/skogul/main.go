b4842d55ab1537a49b8768e8a3bbb4642f7b1659
Kristian Lyngstol <Kristian.Lyngstol@telenor.com>
2019-09-26 13:43:52 +0200

    Implement man-file generation

--- cmd/skogul/main.go
+++ cmd/skogul/main.go
@@ -32,6 +32,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/KristianLyng/skogul/config"
 	"github.com/KristianLyng/skogul/receiver"
@@ -43,66 +44,258 @@ var fhelp = flag.Bool("help", false, "Print more help")
 var frecvhelp = flag.String("receiver-help", "", "Print extra options for receiver")
 var fsendhelp = flag.String("sender-help", "", "Print extra options for sender")
 var fconf = flag.Bool("show", false, "Print the parsed JSON config instead of starting")
+var fman = flag.Bool("make-man", false, "Output RST documentation suited for rst2man")
+
+func man() {
+	fmt.Print(`
+======
+skogul
+======
+
+------
+Skogul
+------
+
+:Manual section: 1
+:Authors: Kristian Lyngstøl <kly@kly.no>
+
+SYNOPSIS
+========
+
+::
+
+	skogul -file config-file [-help] [-sender-help sender] 
+	       [-receiver-help receiver] [-show] [-make-man]
+
+DESCRIPTION
+===========
+
+Skogul is a framework for normalising and transporting event-oriented data
+between different systems. It operates using a concept of a receiver, handler
+and sender. A receiver will accept (or in special cases: generate) data, it
+will then use a handler to prosess that data, e.g. parse JSON data. The handler
+will forward the processed data to a sender.
+
+Senders come in two distinct but interchangable variants: Storage-oriented
+senders are used to send the data to some external resource, e.g., a time series
+database like InfluxDB. Utility-oriented senders are used to do things like
+route data two multiple senders (e.g.: store it in InfluxDB locally, but also
+forward it to a remote database), handle failures by adding retry-mechanics or
+fallback logic, and much more.
+
+The simplest possible example is to receive data over HTTP, parse it as JSON and
+store it to InfluxDB. A more valuable example is to receive data over insecure
+HTTP locally, batch together a collection of data, then forward it over a
+TLS-encrypted and authenticated HTTPS channel to an other skogul instance in
+a different security domain, which can then store it to disk.
+
+There are more examples in the the "examples/" directory.
+
+Configuration is written as JSON as well, and dynamically parsed.
+
+OPTIONS
+=======
+
+`)
+
+	f := flag.CommandLine
+	f.VisitAll(func(fl *flag.Flag) {
+		s := fmt.Sprintf("-%s", fl.Name)
+		name, usage := flag.UnquoteUsage(fl)
+		if len(name) > 0 {
+			s += " " + name
+		}
+		s += "\n\t"
+		s += strings.ReplaceAll(usage, "\n", "\n\t")
+		if fl.DefValue != "" {
+			s += fmt.Sprintf(" (default %v)", fl.DefValue)
+		}
+		fmt.Print(s, "\n\n")
+	})
+
+	fmt.Print(`
+SENDERS
+=======
+
+Senders move parsed data around according to internal logic. A sender will typically
+either ensure data is stored, or forward it according to some internal logic to an
+other sender with that goal.
+
+The following senders exist. A list can also be retrieved by using the "-help"
+option, and -sender-help or -receiver-help.
+
+`)
+	for idx := range sender.Auto {
+		sh, _ := config.HelpSender(idx)
+		thingMan(sh)
+	}
+	fmt.Print(`
+RECEIVERS
+=========
+
+Receivers accept data from the outside world - or in special cases,
+generate the data themself. Receivers do not typically deal with how
+individual collections of data is handled, but leaves that specific task
+to a handler.
+
+The following receivers exist.
+
+`)
+	for idx := range receiver.Auto {
+		sh, _ := config.HelpReceiver(idx)
+		thingMan(sh)
+	}
+	fmt.Print(`
+HANDLERS
+========
+
+There is only one type of handler. It accepts three arguments: A parser to
+parse data, a list of optional transformers, and the first sender that will
+receive the parsed container(s).
+
+Currently the only valid parser is "json" and the only valid transformer is
+"templating".
+
+FIXME: Templating
+
+JSON FORMAT
+===========
+
+Data sent to Skogul will be parsed to fit the internal data model of Skogul. The
+JSON representation is roughly thus::
+
+	{
+		"template": { 
+			"timestamp": "iso8601-time",
+			"metadata": { 
+				"key": value, 
+				...
+			},
+			"data": {
+				"key": value,
+				...
+			}
+		},
+		"metrics": [
+			{
+				"timestamp": "iso8601-time",
+				"metadata": { 
+					"key": value, 
+					...
+				},
+				"data": {
+					"key": value,
+					...
+				}
+			},
+			{ ...}
+		]
+	}
+
+The entire "template" is optional. If the "templater" transformer is
+applied, all metrics will start with whatever value is present in the
+template, and then overwrite with "local" variables. E.g.: If all your
+metrics share timestamp in a collection, you can specify that in the
+template. Or if they share some metadata.
+
+The primary difference between metadata and data is searchability,
+and it will depend on storage engines. Typically this means the name
+of a server is metadata, but the load average is data. Skogul itself
+does not much care.
+
+CONFIGURATION
+=============
+
+Configuration of skogul is done with a json config file, referenced with
+the -file option. You need to specify at least one receiver, handler and
+sender to make something sensible.
+
+EXAMPLES
+========
+
+The following specifies an insecure HTTP-based receiver that will wait up
+to 5 seconds or 1000 metrics before writing data to InfluxDB::
+
+	{
+	  "receivers": {
+	      "api": {
+		"type": "http",
+		"address": "[::1]:8080",
+		"handlers": {
+		  "/": "jsontemplating"
+		}
+	      }
+	  },
+	  "handlers": {
+	    "jsontemplating": {
+	      "parser": "json",
+	      "transformers": [ "templater" ],
+	      "sender": "batch"
+	    }
+	  },
+	  "senders": {
+	    "batch": {
+	      "type": "batch",
+	      "interval": "5s",
+	      "threshold": 1000,
+	      "next": "influx"
+	    },
+	    "influx": {
+	      "type": "influx",
+	      "URL": "http://[::1]:8086/write?db=testdb",
+	      "measurement": "demo",
+	      "Timeout": "10s"
+	    }
+	  }
+	}
+
+More examples are provided in the examples/ directory of the Skogul source
+package.
+
+SEE ALSO
+========
+
+https://github.com/KristianLyng/skogul
+
+BUGS
+====
+
+The biggest known issue right now is that the configuration engine is a bit
+horrible at giving constructive error message, and will silently ignore
+unkown (or misspelled) variable names. Work in progress.
+
+A tip for working around this is to compare your configuration with what
+skogul outputs when you run it with -show, as that is a representation of
+the parsed configuration.
+
+COPYRIGHT
+=========
+
+This document is licensed under the same license as Skogul itself, which
+happens to be GPLv2 (or later). See LICENSE for details.
+
+* Copyright (c) 2019 - Telenor Norge AS
+
+
+`)
+
+}
+
+func thingMan(thing config.Help) {
+	fmt.Printf("%s\n", thing.Name)
+	for l := len(thing.Name); l > 0; l-- {
+		fmt.Print("-")
+	}
+	fmt.Printf("\n\n%s\nSettings:\n\n", thing.Doc)
+	for n, f := range thing.Fields {
+		fmt.Printf("%s\n\t", n)
+		fmt.Printf("[%s] ", f.Type)
+		fmt.Printf("%s %s\n\n", f.Doc, f.Example)
+	}
+}
 
 func help() {
 	flag.Usage()
-	fmt.Println(`
-Skogul provides a set of receivers and senders of time-based data, and is
-intended to bridge different systems together. In its simplest form, it will
-read from, e.g. HTTP, and write to a database like InfluxDB. But it can also
-be configured for considerably more complex chains, including failover, data
-duplication, retries, batching of multiple data sets, and more.
-
-It is designed to be very fast, and flexible.
-
-To start skogul, you need to specify a chain. A chain starts with a
-receiver - this is where data originates as far as Skogul is concerned.
-Each receiver references one or more handler. A handler defines how
-the raw data is parsed (currently only a JSON parser is supported),
-any transformations of data (e.g.: filtering? Today, only templating
-is supported) and the sender that will receive the data.
-
-A sender accepts containers of metrics and Does Something With Them.
-The most traditional senders will just write the data to a database.
-Several databases are supported, including InfluxDB, MySQL, M&R and
-more.
-
-Other senders are meant to provide configuration options. For example,
-you might want to write data to both MySQL and InfluxDB - this can be
-achieved by sending to a "dupe" sender, which in turn will pass data
-to both an InfluxDB-sender and a MySQL-sender. Other senders can be
-used to batch data together before it is forwarded to storage, or
-provide alternate paths if the preferred sender fails, etc. The list is
-long.
-
-A small example of a simple chain is provided here. See the individual
-senders and receivers for more help, e.g. using -sender-help and
--receiver-help.
-
-Semi-complete example:
-
-{
-  "senders": {
-    "print": {
-      "type": "debug"
-    }
-  },
-  "receivers": {
-    "api": {
-      "type": "http",
-      "handlers": {
-	"/": "api_handler"
-      }
-    }
-  },
-  "handlers": {
-    "api_handler": {
-      "parser": "json",
-      "transformers": ["templater"],
-      "sender": "print"
-    }
-  }
-}`)
 	fmt.Println("\nSenders:")
 	for idx, sen := range sender.Auto {
 		config.PrettyPrint(idx, sen.Help)
@@ -111,12 +304,17 @@ Semi-complete example:
 	for idx, rcv := range receiver.Auto {
 		config.PrettyPrint(idx, rcv.Help)
 	}
+	fmt.Println("\nYou can also see the skogul manual page. It can be generated with `./skogul -make-man > foo; rst2man < foo > skogul.1; man ./skogul.1'.")
 }
 
 func main() {
 	flag.Parse()
 	if *fhelp {
 		help()
+		os.Exit(0)
+	}
+	if *fman {
+		man()
 		os.Exit(0)
 	}
 	if *fsendhelp != "" {
