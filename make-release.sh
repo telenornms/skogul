fed431281676201981044cbf253c22fe496f7711
Kristian Lyngstol <Kristian.Lyngstol@telenor.com>
2019-11-18 16:28:48 +0100

    Attempt to fix drone automated release notes

--- make-release.sh
+++ make-release.sh
@@ -21,5 +21,30 @@ rst2man < dist/share/doc/skogul/skogul.rst > dist/share/man/man1/skogul.1
 cp skogul dist/bin
 cp LICENSE dist/share/doc/skogul
 cp -r README.rst docs/* dist/share/doc/skogul/
+
+awk < docs/NEWS -v ver="$V" '
+$0 == ver {
+	here=1
+	herel=length($0)
+	next
+}
+here == 1 && herel == length($0) && /^=+$/ {
+	here = 2
+	next
+}
+here == 1 {
+	here=0
+}
+
+/^v[0-9]+\.[0-9]+\.[0-9]+[0-9a-z]*$/ {
+	here = 0
+}
+
+here == 2 && /.+/ {
+	lines++
+}
+
+here == 2 && lines
+' > notes
 cd dist
 tar cvjf ../skogul-${V}.${OS}-${ARCH}.tar.bz2 .
