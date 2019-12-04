275d7b84bc695438a34c22b2ae4c17863a72c2a8
Håkon Solbjørg <hakon@solbj.org>
2019-12-04 14:19:58 +0100

    fix: Set _unitdir if rpm macro doesn't resolve it

--- make-rpm.sh
+++ make-rpm.sh
@@ -109,9 +109,25 @@ git archive -o rpm-prep/SOURCES/$V.tar.gz --prefix="skogul-$VERSION_NO/" --forma
 echo "Building"
 
 cd rpm-prep
-rpmbuild --bb \
-    --define "_rpmdir $(pwd)" \
-    --define "_sourcedir $(pwd)/SOURCES" \
-    --define "_topdir $(pwd)" \
-    --buildroot "$(pwd)/BUILDROOT" \
-    SPECS/skogul.spec
+
+# Taken from CentOS Linux release 7.6.1810 (Core)
+DEFAULT_UNIT_DIR=/usr/lib/systemd/system
+RPM_UNIT_DIR=$(rpm --eval %{_unitdir})
+if [ "${RPM_UNIT_DIR}" = "%{_unitdir}" ]; then
+    echo "_unitdir not set, setting _unitdir to $DEFAULT_UNIT_DIR"
+
+    rpmbuild --bb \
+        --define "_rpmdir $(pwd)" \
+        --define "_sourcedir $(pwd)/SOURCES" \
+        --define "_topdir $(pwd)" \
+        --define "_unitdir $DEFAULT_UNIT_DIR" \
+        --buildroot "$(pwd)/BUILDROOT" \
+        SPECS/skogul.spec
+else
+    rpmbuild --bb \
+            --define "_rpmdir $(pwd)" \
+            --define "_sourcedir $(pwd)/SOURCES" \
+            --define "_topdir $(pwd)" \
+            --buildroot "$(pwd)/BUILDROOT" \
+            SPECS/skogul.spec
+fi
