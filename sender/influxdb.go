a5fcf1e3bc24a01f52270d50dc0abbf0563986a3
Kristian Lyngstol <Kristian.Lyngstol@telenor.com>
2020-01-20 15:29:52 +0100

    Improve InfluxDB error handling

--- sender/influxdb.go
+++ sender/influxdb.go
@@ -26,13 +26,18 @@ package sender
 import (
 	"bytes"
 	"fmt"
+	"github.com/sirupsen/logrus"
+	"io"
 	"net/http"
+	"reflect"
 	"strings"
 	"sync"
 	"time"
 
 	"github.com/telenornms/skogul"
 )
+
+var influxLog = skogul.Logger("sender", "influxdb")
 
 /*
 InfluxDB posts data to the provided URL and measurement, using the InfluxDB
@@ -48,6 +53,22 @@ type InfluxDB struct {
 	once                    sync.Once
 }
 
+func checkVariable(category string, field string, idx string, value interface{}) error {
+	t := reflect.TypeOf(value)
+	k := t.Kind()
+	if k == reflect.Struct || k == reflect.Array || k == reflect.Slice {
+		influxLog.WithFields(logrus.Fields{
+			"category": category,
+			"field":    field,
+			"index":    idx,
+			"kind":     k,
+		}).Info("Invalid tag/field data type. Flatten/convert data first.")
+		e := skogul.Error{Source: "influxdb sender", Reason: fmt.Sprintf("bad tag/field")}
+		return e
+	}
+	return nil
+}
+
 // Send data to Influx, re-using idb.client.
 func (idb *InfluxDB) Send(c *skogul.Container) error {
 	var buffer bytes.Buffer
@@ -58,8 +79,16 @@ func (idb *InfluxDB) Send(c *skogul.Container) error {
 		}
 		idb.client = &http.Client{Timeout: idb.Timeout.Duration}
 	})
+	added := 0
+	nmdata := 0
+	ndata := 0
 	for _, m := range c.Metrics {
 		measurement := idb.Measurement
+		if len(m.Data) == 0 {
+			// must have SOME data
+			// XXX: Should rneport.
+			continue
+		}
 		if idb.MeasurementFromMetadata != "" {
 			measure, ok := m.Metadata[idb.MeasurementFromMetadata].(string)
 			if ok {
@@ -78,6 +107,24 @@ func (idb *InfluxDB) Send(c *skogul.Container) error {
 				continue
 			}
 		}
+		failed := 0
+		for key, value := range m.Metadata {
+			e1 := checkVariable("metadata", "key", "0", key)
+			e2 := checkVariable("metadata", "value", key, value)
+			if e1 != nil || e2 != nil {
+				failed++
+			}
+		}
+		for key, value := range m.Data {
+			e1 := checkVariable("data", "key", "0", key)
+			e2 := checkVariable("data", "value", key, value)
+			if e1 != nil || e2 != nil {
+				failed++
+			}
+		}
+		if failed > 0 {
+			continue
+		}
 		fmt.Fprintf(&buffer, "%s", measurement)
 		for key, value := range m.Metadata {
 			var field interface{}
@@ -88,22 +135,50 @@ func (idb *InfluxDB) Send(c *skogul.Container) error {
 				field = value
 			}
 			fmt.Fprintf(&buffer, ",%s=%v", idb.replacer.Replace(key), field)
+			nmdata++
 		}
 		fmt.Fprintf(&buffer, " ")
 		comma := ""
 		for key, value := range m.Data {
+
 			fmt.Fprintf(&buffer, "%s%s=%#v", comma, idb.replacer.Replace(key), value)
 			comma = ","
+			ndata++
 		}
 		fmt.Fprintf(&buffer, " %d\n", m.Time.UnixNano())
+		added++
 	}
+	if added == 0 {
+		influxLog.Trace("Tried to send 0 metrics to influx. Probably no viable metrics after filtering out invalid tags and such. You may have to transform your data.")
+		return nil
+	}
+
 	resp, err := idb.client.Post(idb.URL, "text/plain", &buffer)
 	if err != nil {
 		e := skogul.Error{Source: "influxdb sender", Reason: "unable to POST data", Next: err}
+		influxLog.Trace(e)
 		return e
 	}
 	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		e := skogul.Error{Source: "influxdb sender", Reason: fmt.Sprintf("bad response from InfluxDB: %s - %s", resp.Status, resp.Body)}
+		var body []byte
+		if resp.ContentLength > 0 {
+			body = make([]byte, resp.ContentLength)
+
+			if _, err := io.ReadFull(resp.Body, body); err != nil {
+				body = []byte(`unable to ready body`)
+			}
+		} else {
+			body = []byte(fmt.Sprintf("No reply body. Request: %s", buffer.Bytes()))
+		}
+
+		influxLog.WithFields(logrus.Fields{
+			"metricsSent": added,
+			"body":        string(body),
+			"status":      resp.Status,
+			"tags":        nmdata,
+			"fields":      ndata,
+		}).Error("Failed to send data to influx. Bad response from backend.")
+		e := skogul.Error{Source: "influxdb sender", Reason: fmt.Sprintf("bad response from InfluxDB: %s - %s", resp.Status, string(body))}
 		return e
 	}
 	return nil
