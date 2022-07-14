4c3a5418c35acc044a0c624714c660c2c2c86058
Roshini Narasimha Raghavan <107004432+RoshiniNara@users.noreply.github.com>
2022-07-14 10:48:14 +0200

    Update auto.go

--- encoder/auto.go
+++ encoder/auto.go
@@ -4,7 +4,7 @@
  * Copyright (c) 2022 Telenor Norge AS
  * Author(s):
  *  - Roshini NarasimhaRaghavan(roshiragavi@gmail.com)
- *
+ *  - Kristian Lyngstøl <kly@kly.no>
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
