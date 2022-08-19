cb08c54f24ed690ec883328f335915fc9d1dc9ad
RoshiniNara <roshiragavi@gmail.com>
2022-08-19 08:16:26 +0200

    avro test failes due to incorrect arguments

--- encoder/avro_test.go
+++ encoder/avro_test.go
@@ -36,12 +36,13 @@ import (
 // TestAVROEncode tests encoding of a simple avro format from a skogul
 // container
 func TestAVROEncode(t *testing.T) {
-	testAVRO(t, "./testdata/avro_testdata.json", true)
+	by := []byte("{\"metrics\":[{\"timestamp\":\"0001-01-01T00:00:00Z\",\"metadata\":{\"key\":\"value\"}}]}")
+	testAVRO(t, by, true)
 }
 
-func testAVRO(t *testing.T, file string, match bool) {
+func testAVRO(t *testing.T, by byte[], match bool) {
 	t.Helper()
-	c, orig := parseAVRO(t, file)
+	c, orig := parseAVRO(t, by)
 	b, err := encoder.AVRO{}.Encode(c)
 
 	if err != nil {
@@ -75,10 +76,52 @@ func testAVRO(t *testing.T, file string, match bool) {
 
 }
 
-func parseAVRO(t *testing.T, file string) (*skogul.Container, []byte) {
+func parseAVRO(t *testing.T, by byte[]) (*skogul.Container, []byte) {
 	t.Helper()
 
-	b, err := ioutil.ReadFile(file)
+	data_container := skogul.Container{}
+	json.Unmarshal(by, &data_container)
+	schema, err := avro.Parse(`{
+	    "type": "record",
+	    "name": "simple",
+	    "namespace": "org.hamba.avro",
+	    "values": "string",
+	    "fields": [
+	    	{
+			"name": "Metrics", "type": {
+				"type": "array",
+				"items": {
+					"type": "record",
+					"name": "metrics",
+					"fields": [
+						{
+							"name": "Metadata",
+							"type": {
+								"type": "map",
+								"values": ["string", "int", "long", "float", "double" ]
+							}
+						},
+						{
+							"name": "Data",
+							"type": {
+								"type": "map",
+								"values": ["string", "int", "long", "float", "double" ]
+							}
+						}
+					]
+				}
+			}
+		}
+	]
+}
+`)
+	if err != nil {
+		t.Logf("Failed to parse AVRO schema and test data preparation error: %v", err)
+		t.FailNow()
+
+	}
+
+	b, err := avro.Marshal(schema, data_container)
 
 	if err != nil {
 		t.Logf("Failed to read test data file: %v", err)
