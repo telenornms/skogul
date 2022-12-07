02c17e7f4c2313580b7b63ee2a5fe6a0866c0d57
n-holmstedt <n.holmstedt@gmail.com>
2022-12-07 10:32:37 +0100

    Add ability to append multiple metadata fields to subject

--- sender/nats.go
+++ sender/nats.go
@@ -40,18 +40,18 @@ Authentication & Authorization features in the nats golang client.
 */
 
 type Nats struct {
-	Servers       string `doc:"Comma separated list of nats URLs"`
-	Subject       string `doc:"Subject to publish messages on"`
-	SubjectAppend string `doc:"Append this Metadata field to subject"`
-	Name          string `doc:"Client name"`
-	Username      string `doc:"Client username"`
-	Password      string `doc:"Client password"`
-	TLSClientKey  string `doc:"TLS client key file path"`
-	TLSClientCert string `doc:"TLS client cert file path"`
-	TLSCACert     string `doc:"CA cert file path"`
-	UserCreds     string `doc:"Nats credentials file path"`
-	NKeyFile      string `doc:"Nats nkey file path"`
-	Insecure      bool   `doc:"TLS InsecureSkipVerify"`
+	Servers       string   `doc:"Comma separated list of nats URLs"`
+	Subject       string   `doc:"Subject to publish messages on"`
+	SubjectAppend []string `doc:"Append theese Metadata fields to subject"`
+	Name          string   `doc:"Client name"`
+	Username      string   `doc:"Client username"`
+	Password      string   `doc:"Client password"`
+	TLSClientKey  string   `doc:"TLS client key file path"`
+	TLSClientCert string   `doc:"TLS client cert file path"`
+	TLSCACert     string   `doc:"CA cert file path"`
+	UserCreds     string   `doc:"Nats credentials file path"`
+	NKeyFile      string   `doc:"Nats nkey file path"`
+	Insecure      bool     `doc:"TLS InsecureSkipVerify"`
 	Encoder       skogul.EncoderRef
 	conOpts       *[]nats.Option
 	natsCon       *nats.Conn
@@ -160,13 +160,17 @@ func (n *Nats) Send(c *skogul.Container) error {
 	}
 	for i, m := range c.Metrics {
 		subject := n.Subject
-		a_subject := m.Metadata[n.SubjectAppend]
-		//Append metadata field to subject.
-		if n.SubjectAppend != "" && a_subject.(string) != "" {
-			if !strings.HasSuffix(m.Metadata[n.SubjectAppend].(string), ".") {
-				subject = subject + "."
+		//Append metadata fields to subject.
+		for _, value := range n.SubjectAppend {
+			if appSubject, ok := m.Metadata[value]; ok {
+				if appSubject.(string) == "" {
+					continue
+				}
+				if !strings.HasPrefix(appSubject.(string), ".") {
+					appSubject = "." + appSubject.(string)
+				}
+				subject = subject + appSubject.(string)
 			}
-			subject = subject + m.Metadata[n.SubjectAppend].(string)
 		}
 
 		b, err := n.Encoder.E.EncodeMetric(m)
