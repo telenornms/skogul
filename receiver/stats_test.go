6f31d4171440d99850586ef1c2ae63c66b7eca30
Kamil <24246260+kamilernerd@users.noreply.github.com>
2025-04-11 15:16:35 +0200

    Fix test

--- receiver/stats_test.go
+++ receiver/stats_test.go
@@ -133,7 +133,10 @@ func TestStatsDoesntBlockChan(t *testing.T) {
 		t.Errorf("expected to have gotten 100 stats container but got %d", tester.Received())
 	}
 
-	if td > time.Millisecond*1 {
+	// this test being time sensitive might cause issues when
+	// running in a slow environment.
+	// wait for up to 15ms and fail when exceeded
+	if td > time.Millisecond*15 {
 		t.Errorf("expected stats channel to not block noticeably, but had to wait %v", td)
 	}
 }
