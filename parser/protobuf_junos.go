d488c90f5499ba307d3e64d324abd7e7a0ad4891
Aslak <aslak.bakkeland@telenor.no>
2025-11-26 14:45:38 +0100

    fix: remove duplicate Parsed counter increment

--- parser/protobuf_junos.go
+++ parser/protobuf_junos.go
@@ -270,7 +270,6 @@ func (x *ProtoBuf) createData(telemetry *pb.TelemetryStream) (map[string]interfa
 	delete(metrics, "componentId")
 	delete(metrics, "subComponentId")
 
-	atomic.AddUint64(&x.stats.Parsed, 1)
 	return metrics, nil
 }
 
