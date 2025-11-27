c9c67d4f4e42eab3cb4c3350c6864ab9d4a2cfc4
Aslak <aslak.bakkeland@telenor.no>
2025-11-27 23:19:19 +0100

    refactor: remove dependency on buf

--- tools.go
+++ tools.go
@@ -2,11 +2,11 @@
 // +build tools
 
 // Package tools tracks dependencies for code generation and build tools.
+// These are installed via `go install` in gen/generate.sh.
 // This file ensures that `go mod tidy` won't remove tool dependencies.
 package tools
 
 import (
-	_ "github.com/bufbuild/buf/cmd/buf"
 	_ "github.com/planetscale/vtprotobuf/cmd/protoc-gen-go-vtproto"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
